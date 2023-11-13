34d734d5e3425bbcced59482a5b1d00d7cf03798
-=[ Heisenberg ]=- <roastedcookie47@hotmail.com>
2023-11-13 18:24:18 +0800

    hw5第一版

--- hw5/webook/internal/repository/async_sms_repository.go
+++ hw5/webook/internal/repository/async_sms_repository.go
@@ -2,20 +2,14 @@ package repository
 
 import (
 	"context"
-	"github.com/ecodeclub/ekit/sqlx"
 	"github.com/jasonzhao47/goTrainingCamp/hw5/webook/internal/domain"
 	"github.com/jasonzhao47/goTrainingCamp/hw5/webook/internal/repository/dao"
 )
 
-var ErrWaitingSMSNotFound = dao.ErrWaitingSMSNotFound
-
 //go:generate mockgen -source=./async_sms_repository.go -package=repomocks -destination=mocks/async_sms_repository.mock.go AsyncSmsRepository
 type AsyncSmsRepository interface {
-	// Add 添加一个异步 SMS 记录。
-	// 你叫做 Create 或者 Insert 也可以
 	Add(ctx context.Context, s domain.AsyncSms) error
-	PreemptWaitingSMS(ctx context.Context) (domain.AsyncSms, error)
-	ReportScheduleResult(ctx context.Context, id int64, success bool) error
+	FindFailedRequest(ctx context.Context) ([]domain.AsyncSms, error)
 }
 
 type asyncSmsRepository struct {
@@ -30,35 +24,14 @@ func NewAsyncSMSRepository(dao dao.AsyncSmsDAO) AsyncSmsRepository {
 
 func (a *asyncSmsRepository) Add(ctx context.Context, s domain.AsyncSms) error {
 	return a.dao.Insert(ctx, dao.AsyncSms{
-		Config: sqlx.JsonColumn[dao.SmsConfig]{
-			Val: dao.SmsConfig{
-				TplId:   s.TplId,
-				Args:    s.Args,
-				Numbers: s.Numbers,
-			},
-			Valid: true,
-		},
+		Id:       s.Id,
+		TplId:    s.TplId,
+		Args:     s.Args,
+		Numbers:  s.Numbers,
 		RetryMax: s.RetryMax,
 	})
 }
 
-func (a *asyncSmsRepository) PreemptWaitingSMS(ctx context.Context) (domain.AsyncSms, error) {
-	as, err := a.dao.GetWaitingSMS(ctx)
-	if err != nil {
-		return domain.AsyncSms{}, err
-	}
-	return domain.AsyncSms{
-		Id:       as.Id,
-		TplId:    as.Config.Val.TplId,
-		Numbers:  as.Config.Val.Numbers,
-		Args:     as.Config.Val.Args,
-		RetryMax: as.RetryMax,
-	}, nil
-}
-
-func (a *asyncSmsRepository) ReportScheduleResult(ctx context.Context, id int64, success bool) error {
-	if success {
-		return a.dao.MarkSuccess(ctx, id)
-	}
-	return a.dao.MarkFailed(ctx, id)
+func (a *asyncSmsRepository) FindFailedRequest(ctx context.Context) ([]domain.AsyncSms, error) {
+	return a.dao.GetFailed(ctx)
 }
